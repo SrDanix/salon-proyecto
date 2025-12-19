2dbbb27906604512a78cfea89a61b1f6e7514ca4
DaniloFuentesFernandez <danilo.fuentes06@inacapmail.cl>
2025-12-19 20:26:13 -0300

    actualizar el este

--- salonProyecto/settings.py
+++ salonProyecto/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
